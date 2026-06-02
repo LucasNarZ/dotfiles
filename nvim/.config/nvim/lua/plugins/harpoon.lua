5eb7b1314ef8d74ef385519995b0056692518947
LucasNarZ <lucasnarloch123@gmail.com>
2026-06-02 12:53:40 -0300

    feat: update to harpoon v2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,7 +1,44 @@
 return {
     "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
     config = function()
-        require("harpoon").setup()
-    end
-}
+        local harpoon = require("harpoon")
 
+        harpoon:setup()
+
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end)
+
+        vim.keymap.set("n", "<leader>h", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list(), {
+                border = "rounded",
+                title_pos = "center",
+                ui_width_ratio = 0.55,
+            })
+        end)
+
+        vim.keymap.set("n", "<leader>1", function()
+            harpoon:list():select(1)
+        end)
+
+        vim.keymap.set("n", "<leader>2", function()
+            harpoon:list():select(2)
+        end)
+
+        vim.keymap.set("n", "<leader>3", function()
+            harpoon:list():select(3)
+        end)
+
+        vim.keymap.set("n", "<leader>4", function()
+            harpoon:list():select(4)
+        end)
+
+        vim.keymap.set("n", "<leader>5", function()
+            harpoon:list():select(5)
+        end)
+    end,
+}
