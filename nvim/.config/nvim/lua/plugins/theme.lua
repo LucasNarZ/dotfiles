b9dfc36f4ac441af6b9aa5866572d3d655d03e0f
LucasNarZ <lucasnarloch123@gmail.com>
2026-06-02 14:21:17 -0300

    feat: use github theme

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,13 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function() vim.cmd("colorscheme tokyonight") end 
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+
+    vim.cmd('colorscheme github_dark_dimmed')
+  end,
 }
